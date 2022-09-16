44e10528d749f0ebea4d0ad680f0dd23ae01939a
ji-young-kim-portfolio <113660675+ji-young-kim-portfolio@users.noreply.github.com>
2022-09-16 14:36:19 +0900

    Add files via upload

---
+++ script/scroll-fixed.js
@@ -0,0 +1,16 @@
+$(function() {
+
+  $(document).ready(function() {
+
+    var scrollOffset = $('.nav').offset();
+
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > scrollOffset.top) {
+        $('.nav').addClass('scroll-fixed');
+      }
+      else {
+        $('.nav').removeClass('scroll-fixed');
+      }
+    });
+  });
+});
