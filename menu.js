9069f0e497855bf3a32c6e9c9eb87a5959031e4e
ji-young-kim-portfolio <113660675+ji-young-kim-portfolio@users.noreply.github.com>
2022-10-11 17:15:07 +0900

    Delete menu.js

--- menu.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.nav> li').mouseover(function(){
-        $('.all_box').stop(1500).slideDown();
-    });
-    $('.nav> li').mouseout(function(){
-        $('.all_box').stop(1500).slideUp(500);
-    });
-});
