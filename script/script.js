9ef3fcb69d625f4142d8ab51c37c60f5bf547076
ji-young-kim-portfolio <113660675+ji-young-kim-portfolio@users.noreply.github.com>
2022-10-11 16:55:57 +0900

    Delete script directory

--- script/script.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$("#board li").click(function(){
-		$(this).children("div").slideToggle()
-		$(this).siblings().children("div").slideUp();
-		$(this).toggleClass("on").siblings().removeClass("on")
-	});
-});
